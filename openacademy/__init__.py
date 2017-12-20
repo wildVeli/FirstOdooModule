b560d99e11ac4384f4142fc7fc5ba53bf4dfd4e9
Sergio <ubuntu@ubuntu>
2017-12-20 13:57:37 +0100

    partner

---
+++ openacademy/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+import controllers.controllers
+import models.models
+import models.partner
---
+++ openacademy/models/models.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+    
+class Session (models.Model):
+    _name = 'openacademy.session'
+    
+    name = fields.Char (required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6,2),help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    
+    instructor_id=fields.Many2one('res.partner',string="Instructor")
+    course_id=fields.Many2one('openacademy.course',ondelete='cascade',string="Course",required=True)
+    atendees_ids = fields.Many2many('res.partner',string= "Attendees")
+
+class Course (models.Model):
+    _name = 'openacademy.course'
+    
+    name = fields.Char(string='Title',required=True)
+    description = fields.Text()
+    
+    responsible_id = fields.Many2one('res.users',ondelete='set null', string="Responsible",index=True)
+    sessions_ids=fields.One2many('openacademy.sesion','course_id',string="Sessions")
+    
+    
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
